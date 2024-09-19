bd46dd980fa5d2e293405f25e7391b1bbb6c294e
trishaldoshi <trishaldoshi@gmail.com>
2024-09-19 22:34:40 +0800

    added load pickle function

--- src/tools/common.py
+++ src/tools/common.py
@@ -9,14 +9,22 @@ import numpy as np
 from src.tools.custom_exception import CustomException
 from src.tools.custom_logger import logging
 
+#function for saving pickle file
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
-
     except Exception as e:
+        logging.error(e)
+        raise CustomException(e, sys)
+
+ #function for loading pickle file   
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.error(e)
         raise CustomException(e, sys)
