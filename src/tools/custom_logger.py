16e6e0455bc267406d1ee6c5c496e4c5c4223bf2
trishaldoshi <trishaldoshi@gmail.com>
2024-09-19 21:47:25 +0800

    fixed logging

--- src/tools/custom_logger.py
+++ src/tools/custom_logger.py
@@ -4,8 +4,8 @@ import logging
 import os
 from datetime import datetime
 
-log_file_name=f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",log_file_name)
+log_file_name=f"{datetime.now().strftime('%Y_%m_%d_%H')}.log"
+logs_path=os.path.join(os.getcwd(),"logs")
 os.makedirs(logs_path,exist_ok=True)
 
 log_file_path=os.path.join(logs_path,log_file_name)
